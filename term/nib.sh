0801bf97634aef245e27c7e22651f3951bcbed4e
eastwood <clintryan3@gmail.com>
2016-04-20 10:37:26 +1000

    Updated config

---
+++ term/nib.sh
@@ -0,0 +1,4 @@
+export HTTP_PROXY="http://buildproxy.nibdom.com.au:3128"
+export HTTPS_PROXY="http://buildproxy.nibdom.com.au:3128" 
+export http_proxy="http://buildproxy.nibdom.com.au:3128" 
+export https_proxy="http://buildproxy.nibdom.com.au:3128"
