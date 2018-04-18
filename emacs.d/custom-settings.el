6679c8a5d5748b21c50f5d7b0dc906fa8820bc09
Clint Ryan <clintryan3@gmail.com>
2018-04-18 22:02:21 +1000

    Adds flycheck

--- emacs.d/custom-settings.el
+++ emacs.d/custom-settings.el
@@ -5,7 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" default))))
+    ("e11569fd7e31321a33358ee4b232c2d3cf05caccd90f896e1df6cab228191109" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" default)))
+ '(package-selected-packages
+   (quote
+    (zenburn-theme yasnippet yaml-mode which-key web-mode use-package tide tern-auto-complete solarized-theme smartparens slime rjsx-mode racer projectile org-gcal org-bullets omnisharp npm-mode neotree markdown-mode leuven-theme json-mode flycheck-rust expand-region exec-path-from-shell evil-surround evil-magit evil-leader dracula-theme counsel company-tern cargo ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
