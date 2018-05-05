85727c4f11cfd481dacd2eb6e92391d9f250a10c
clint <clint.ryan3@gmail.com>
2018-05-05 14:31:00 +1000

    Why not auto trigger omni complete mode?

--- emacs.d/custom-settings.el
+++ emacs.d/custom-settings.el
@@ -5,7 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" default))))
+    ("67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" default)))
+ '(package-selected-packages
+   (quote
+    (cider zenburn-theme yasnippet yaml-mode which-key web-mode use-package tide solarized-theme smartparens rjsx-mode racer projectile org-gcal org-bullets omnisharp npm-mode neotree markdown-mode leuven-theme json-mode gruvbox-theme flycheck-rust expand-region exec-path-from-shell evil-surround evil-magit evil-leader dracula-theme diminish counsel company-tern cargo))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
