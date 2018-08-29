18d79b83c84e4f3f782f01aff7a0188cfff4eea1
Clint Ryan <c.ryan@nib.com.au>
2018-08-29 14:36:02 +1000

    Updates some files

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,9 +1,8 @@
 ;;; Package --- Summary
-
 ;;; Commentary:
+
 ;; Emacs init file responsible for either loading a pre-compiled configuration file
 ;; or tangling and loading a literate org configuration file.
-
 ;;; Code:
 
 ;; Don't attempt to find/apply special file handlers to files loaded during startup.
@@ -16,20 +15,3 @@
     (org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))))
 
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" default)))
- '(package-selected-packages
-   (quote
-    (pkg-info zenburn-theme yasnippet yaml-mode which-key web-mode use-package tide solarized-theme smartparens slime rjsx-mode racer projectile ox-reveal org-present org-gcal org-bullets omnisharp npm-mode neotree material-theme markdown-mode leuven-theme json-mode htmlize gruvbox-theme flycheck-rust expand-region exec-path-from-shell evil-surround evil-magit evil-leader dracula-theme cyberpunk-theme counsel company-tern cargo))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
