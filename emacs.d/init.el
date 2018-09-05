2a3426fa15c1d02d1a6bc2ede64bf9671e265a80
Clint Ryan <c.ryan@nib.com.au>
2018-09-06 00:33:50 +1000

    Updates gitignore

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,11 +1,8 @@
 ;;; Package --- Summary
 ;;; Commentary:
-
 ;; Emacs init file responsible for either loading a pre-compiled configuration file
 ;; or tangling and loading a literate org configuration file.
 ;;; Code:
-
-;; Don't attempt to find/apply special file handlers to files loaded during startup.
 (let ((file-name-handler-alist nil))
   ;; If config is pre-compiled, then load that
   (if (file-exists-p (expand-file-name "emacs.elc" user-emacs-directory))
