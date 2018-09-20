47bb8caa179f7b8f6ce1141e79ec5450608a982b
Clint Ryan <c.ryan@nib.com.au>
2018-09-20 19:40:11 +1000

    Cleans up terminal files

--- term/osx-install.sh
+++ term/osx-install.sh
@@ -14,7 +14,8 @@ brew install python@2
 brew install ruby
 brew install reattach-to-user-namespace
 brew install tmuxinator
-brew install node
+brew install nvm
+brew install neovim
 
 # Install config
 cd ~/Workspace/github.com/eastwood/
@@ -29,12 +30,17 @@ ln -s ~/Workspace/github.com/eastwood/config/term/tmuxinator ~/.tmuxinator
 ln -s ~/Workspace/github.com/eastwood/config/term/tmux.conf ~/.tmux.conf
 ln -s ~/Workspace/github.com/eastwood/config/term ~/.config/term/
 
+# Install node and others
+nvm install 8
+npm install -g typescript
+npm install -g javascript-typescript-langserver
+
 # Install nvim
 mkdir -p ~/.config/nvim
-brew install neovim/neovim/neovim
 pip3 install --index-url=http://pypi.python.org/simple/ --trusted-host pypi.python.org neovim
 ln -s ~/Workspace/github.com/eastwood/config/vim/init.vim ~/.config/nvim/init.vim
 
-
 # Install git bash
-ln -s ~/Workspace/github.com/eastwood/config/term/profile ~/.bash_profile
+ln -s ~/Workspace/github.com/eastwood/config/term/.zshenv ~/.zshenv
+ln -s ~/Workspace/github.com/eastwood/config/term/.zshrc ~/.zshrc
+ln -s ~/Workspace/github.com/eastwood/config/term/.oh-my-zsh ~/.oh-my-zsh
