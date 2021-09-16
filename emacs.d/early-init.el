c3947d25a5d38f8555912a01811f265ec6bc9180
Clint Ryan <c.ryan@nib.com.au>
2021-09-16 23:41:15 +1000

    Adds early init

---
+++ emacs.d/early-init.el
@@ -0,0 +1,27 @@
+(setq package-enable-at-startup nil)
+(setq package-native-compile t)
+(setq package-check-signature nil) 
+
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(xterm-mouse-mode 1)
+(tool-bar-mode -1)
+(modify-all-frames-parameters '((vertical-scroll-bars . nil)))
+
+(eval-when-compile
+  (require 'package)
+  (unless (assoc-default "elpa" package-archives)
+    (add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/") t))
+  (unless (assoc-default "melpa" package-archives)
+    (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
+  (unless (assoc-default "org" package-archives)
+    (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t))
+  (package-initialize)
+  (unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
+
+  (require 'use-package)
+  (setq use-package-verbose t
+        use-package-always-defer t
+        use-package-always-ensure t))
