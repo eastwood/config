dde50518375bf62dd89af56123e0ddd5413e991b
eastwd <hello@clintonryan.com>
2023-10-09 01:37:17 +1100

    Updates

--- emacs/custom.el
+++ emacs/custom.el
@@ -85,15 +85,14 @@
       (null-device . "/dev/null"))))
  '(custom-enabled-themes '(nord))
  '(custom-safe-themes
-   '("8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" "2721b06afaf1769ef63f942bf3e977f208f517b187f2526f0e57c1bd4a000350" "da75eceab6bea9298e04ce5b4b07349f8c02da305734f7c0c8c6af7b5eaa9738" "37b6695bae243145fa2dfb41440c204cd22833c25cd1993b0f258905b9e65577" "0c83e0b50946e39e237769ad368a08f2cd1c854ccbcd1a01d39fdce4d6f86478" "29b4f767c48da68f8f3c2bbf0dde2be58e4ed9c97e685af5a7ab7844f0d08b8b" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "3cdd0a96236a9db4e903c01cb45c0c111eb1492313a65790adb894f9f1a33b2d" "6adeb971e4d5fe32bee0d5b1302bc0dfd70d4b42bad61e1c346599a6dc9569b5" "9013233028d9798f901e5e8efb31841c24c12444d3b6e92580080505d56fd392" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "a9eeab09d61fef94084a95f82557e147d9630fbbb82a837f971f83e66e21e5ad" "443e2c3c4dd44510f0ea8247b438e834188dc1c6fb80785d83ad3628eadf9294" "4c7228157ba3a48c288ad8ef83c490b94cb29ef01236205e360c2c4db200bb18" default))
+   '("4c7228157ba3a48c288ad8ef83c490b94cb29ef01236205e360c2c4db200bb18" default))
  '(initial-frame-alist '((fullscreen . maximized)))
  '(package-selected-packages
-   '(zenburn-theme zenburn elfeed rg expand-region doom-modeline corfu eglot perspective xclip which-key vterm projectile nord-theme no-littering multiple-cursors move-text markdown-mode magit goto-chg god-mode git-link exec-path-from-shell eglot-fsharp all-the-icons)))
+   '(elfeed rg expand-region doom-modeline corfu eglot perspective xclip which-key vterm projectile nord-theme no-littering multiple-cursors move-text markdown-mode magit goto-chg god-mode git-link exec-path-from-shell eglot-fsharp all-the-icons)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(doom-modeline-info ((t (:foreground "black"))))
- '(doom-modeline-project-dir ((t (:foreground "saddle brown")))))
+ )
--- emacs/init.el
+++ emacs/init.el
@@ -1,8 +1,3 @@
-;;; package --- Summary
-;;; My nice vanilla config, use as much defaults as possible
-;;; Commentary:
-;;; Is good
-;;; Code
 (setq user-full-name "Clinton Ryan"
       user-mail-address "hello@clintonryan.com")
 
@@ -33,8 +28,8 @@
 (set-fontset-font t 'symbol "Apple Color Emoji")
 
 (setq custom-file "~/.config/emacs/custom.el")
-(setq-default native-comp-async-report-warnings-errors nil)
-(setq-default native-comp-deferred-compilation t)
+(setq native-comp-async-report-warnings-errors nil)
+(setq native-comp-deferred-compilation t)
 (setq use-package-always-ensure t)
 (setq inhibit-startup-message t)
 (setq visible-bell t)
@@ -49,43 +44,37 @@
   (global-set-key (kbd "C-S-z") 'undo-redo))
 
 (use-package god-mode
-  :defines god-local-mode-map god-mode-enable-function-key-translation god-exempt-major-modes god-local-mode
-  :functions god-mode-all my-god-mode-update-cursor-type god-local-mode
-  :init
+  :config
   (setq god-mode-enable-function-key-translation nil)
   (setq god-exempt-major-modes '(vterm-mode info-mode))
-  :config
   (define-key god-local-mode-map (kbd ".") #'repeat)
   (define-key god-local-mode-map (kbd "i") #'god-mode-all)
   (define-key god-local-mode-map (kbd "[") #'backward-paragraph)
   (define-key god-local-mode-map (kbd "]") #'forward-paragraph)
   ;; this is a nice addition to making sure that the cursor changes for visual help
   (defun my-god-mode-update-cursor-type ()
+    (setq god-mode-enable-function-key-translation nil)
     (setq cursor-type (if (or god-local-mode buffer-read-only) 'box 'bar)))
+
   (add-hook 'post-command-hook #'my-god-mode-update-cursor-type))
 
 (use-package which-key
-  :functions which-key-mode
   :config
   (which-key-mode))
 
 (defun my/kill-magit-buffer ()
-  "Kill magit buffers and all within project."
   (interactive)
-  (setq current-prefix-arg '(16))
-  (magit-mode-bury-buffer))
+  (magit-mode-bury-buffer 16))
 
 (use-package magit
-  :defines magit-mode-map
-  :functions magit-mode-bury-buffer
   :commands (magit-status)
   :config
   (define-key magit-mode-map (kbd "q") 'my/kill-magit-buffer))
 
 (use-package git-link
-  :custom
-  (git-link-open-in-browser t)
-  :commands (git-link))
+  :commands (git-link)
+  :config
+  (setq git-link-open-in-browser t))
 
 (defun my/open-jira()
   "Open JIRA in browser."
@@ -100,15 +89,13 @@
     (browse-url (concat "https://buildkite.com/nib-health-funds-ltd/" name))))
 
 (defun open-config()
-  "Open config file."
   (interactive)
   (find-file "~/.config/emacs/init.el"))
 
 (use-package corfu
-  :defines corfu-auto
-  :functions global-corfu-mode
+  :custom
+  (corfu-auto t) 
   :config
-  (setq corfu-auto t)
   (setq tab-always-indent 'complete)
   (global-corfu-mode))
 
@@ -116,19 +103,17 @@
   :commands (exec-path-from-shell-initialize))
 
 (use-package projectile
-  :defines projectile-mode-map
-  :functions projectile-mode projectile-project-name
   :commands (projectile-switch-project)
-  :config
+  :init
   (projectile-mode)
+  :config
   (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
   (define-key projectile-mode-map (kbd "C-S-f") 'projectile-ripgrep)
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
 (use-package perspective
-  :functions persp-mode
-  :custom
-  (persp-suppress-no-prefix-key-warning t)
+  :init
+  (setq persp-suppress-no-prefix-key-warning t)
   :config
   (persp-mode))
 
@@ -152,7 +137,6 @@
   (setq-default typescript-indent-level 2))
 
 (use-package elfeed
-  :defines elfeed-feeds
   :config
   (setq elfeed-feeds
 	'(("https://reddit.com/r/programming/.rss" programming)
@@ -165,14 +149,13 @@
 (use-package vterm)
 (use-package doom-modeline)
 (use-package rg)
-(use-package markdown-mode)
 
 ;; You'll need to download wl-clipboard to get this working for
 ;; WSL2. Otherwise, there's some really shit freezes and experiences.
 ;; This will also make it so that have a separate process running
 ;; Called wl-copy in the background, which you'll need to exit
 (defun my/configure-wayland-clipboard()
-    (setq wl-copy-process nil)
+    (setq wl-copy-process nil)    
     (defun wl-copy (text)
       (setq wl-copy-process
 	    (make-process :name "wl-copy"
@@ -199,8 +182,8 @@
 (global-set-key (kbd "C-x C-o") #'other-window)
 (global-set-key (kbd "<escape>") (lambda () (interactive) (god-local-mode t)))
 (global-set-key (kbd "C-.") #'eglot-code-actions)
-(global-set-key (kbd "M-<up>") 'flymake-goto-prev-error)
-(global-set-key (kbd "M-<down>") 'flymake-goto-next-error)
+(global-set-key (kbd "M-<up>") 'backward-paragraph)
+(global-set-key (kbd "M-<down>") 'forward-paragraph)
 (global-set-key (kbd "C-<up>") 'move-text-up)
 (global-set-key (kbd "C-<down>") 'move-text-down)
 (global-set-key (kbd "s-<up>") 'backward-paragraph)
@@ -209,14 +192,10 @@
 (global-set-key (kbd "C-M-<down>") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-M-<left>") 'mc/mark-all-like-this)
 (global-set-key (kbd "C-M-<right>") 'mc/skip-to-next-like-this)
-(global-set-key (kbd "M-n") 'forward-paragraph)
-(global-set-key (kbd "M-p") 'backward-paragraph)
 (global-set-key (kbd "<f12>") 'persp-switch)
 (global-set-key (kbd "C-=") 'er/expand-region)
-(global-set-key (kbd "C-c p p") 'projectile-switch-project)
+(global-set-key (kbd "M-n") 'flymake-goto-next-error)
+(global-set-key (kbd "M-p") 'flymake-goto-prev-error)
 
 (load custom-file)
-(load-theme 'leuven-dark t)
-
-(provide 'init)
-;;; init.el ends here
+(load-theme 'nord t)
