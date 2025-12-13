e0487303fb949f943e228fe82e725d71e32c0881
Clint <hello@clintonryan.com>
2025-12-14 01:11:49 +1100

    Remove old files

--- vsc/vsc-nib/src/commands/index.ts
+++
@@ -1,6 +0,0 @@
-export { rqpAuth } from './rqpAuth';
-export { cloneRepo } from './cloneRepo';
-export { openSalesZoom } from './openSalesZoom';
-export { openJira } from './openJira';
-export { openBuildkite } from './openBuildkite';
-export { createReviewNotes } from './createReviewNotes';
